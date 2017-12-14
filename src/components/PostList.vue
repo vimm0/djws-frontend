f174e9395a7baff227b2e5f3a581ac66c8f1c8d4
Sandesh <vimmrana0@gmail.com>
2017-12-14 12:53:24 +0545

    Eslint fixes

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,53 +1,50 @@
 <template>
   <div id="post-list">
     <div class="container">
-
-            <v-flex>
-              <div v-for="post in posts" :key="post.title">
-                <v-card class="my-3" hover>
-                  <v-card-media
-                    class="white--text"
-                    height="170px"
-                    src='https://opendata.cityofnewyork.us/wp-content/uploads/2016/12/placeholder-1200x400.svg'>
-                    <v-container fill-height fluid>
-                      <v-layout fill-height>
-                        <v-flex xs12 align-end flexbox>
-                          <span class="headline"><router-link :to="{ name: 'PostDetail', params: { postId: post.id }}">{{ post.title }}</router-link>
+      <v-flex>
+        <div v-for="post in posts" :key="post.title">
+          <v-card class="my-3" hover>
+            <v-card-media
+              class="white--text"
+              height="170px"
+              src='https://opendata.cityofnewyork.us/wp-content/uploads/2016/12/placeholder-1200x400.svg'>
+              <v-container fill-height fluid>
+                <v-layout fill-height>
+                  <v-flex xs12 align-end flexbox>
+                          <span class="headline"><router-link :to="{ name: 'PostDetail', params: { postId: post.id }}">{{ post.title
+                            }}</router-link>
                           </span>
-
-                        </v-flex>
-                      </v-layout>
-                    </v-container>
-                  </v-card-media>
-                  <v-card-text>
-                      <small>Published: {{ post.publish }}</small>
-
-                      <h3 v-if="draft">Staff only: Draft</h3>
-        <!--if more than published today-->
-        <h3 v-if="getFuturePost">Staff Only: Future Post</h3>
-        <!--endif-->
-
-        <p>Author: {{ post.author }}</p>
-                    {{ post.content| turncate(400) }}
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-btn icon class="red--text">
-                      <v-icon medium>fa-reddit</v-icon>
-                    </v-btn>
-                    <v-btn icon class="light-blue--text">
-                      <v-icon medium>fa-twitter</v-icon>
-                    </v-btn>
-                    <v-btn icon class="blue--text text--darken-4">
-                      <v-icon medium>fa-facebook</v-icon>
-                    </v-btn>
-                    <v-spacer></v-spacer>
-                    <router-link :to="{ name: 'PostDetail', params: { postId: post.id }}">
-                      <v-btn flat class="blue--text">Read More</v-btn>
-                    </router-link>
-                  </v-card-actions>
-                </v-card>
-              </div>
-            </v-flex>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-media>
+            <v-card-text>
+              <small>Published: {{ post.publish }}</small>
+              <h3 v-if="draft">Staff only: Draft</h3>
+              <!--if more than published today-->
+              <h3 v-if="getFuturePost">Staff Only: Future Post</h3>
+              <!--endif-->
+              <p>Author: {{ post.author }}</p>
+              {{ post.content | turncate(400) }}
+            </v-card-text>
+            <v-card-actions>
+              <v-btn icon class="red--text">
+                <v-icon medium>fa-reddit</v-icon>
+              </v-btn>
+              <v-btn icon class="light-blue--text">
+                <v-icon medium>fa-twitter</v-icon>
+              </v-btn>
+              <v-btn icon class="blue--text text--darken-4">
+                <v-icon medium>fa-facebook</v-icon>
+              </v-btn>
+              <v-spacer></v-spacer>
+              <router-link :to="{ name: 'PostDetail', params: { postId: post.id }}">
+                <v-btn flat class="blue--text">Read More</v-btn>
+              </router-link>
+            </v-card-actions>
+          </v-card>
+        </div>
+      </v-flex>
       <!-- <div class="post sitemap" v-for="post in posts">
         <img src='https://opendata.cityofnewyork.us/wp-content/uploads/2016/12/placeholder-1200x400.svg'
              class='img-responsive'/>
@@ -118,11 +115,12 @@
   }
 </script>
 <style lang="scss" scoped>
-.headline{
-  color:#000;
-}
-a:hover {
+  .headline {
+    color: #000;
+  }
+
+  a:hover {
     color: #0056b3;
     text-decoration: none;
-}
+  }
 </style>
