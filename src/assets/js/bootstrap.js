d6feb27bc2e285db3df152061fce2f8605c9e24d
vimm0 <vimmrana0@gmail.com>
2018-08-18 17:59:32 +0545

    Build new file, check vulnerabilities and add bootstrap with cdn

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -1,7 +1,7 @@
-global.jQuery = require('jquery')
-global.$ = global.jQuery
-global.Tether = require('tether')
-require('bootstrap')
-
+// global.jQuery = require('jquery')
+// global.$ = global.jQuery
+// global.Tether = require('tether')
+// require('bootstrap')
+// TODO: Fix bootstrap dependency for now cdn is used
 // require('vuetify/dist/vuetify.min.js')
 require('vuetify/dist/vuetify.min.css')
